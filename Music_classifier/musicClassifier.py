3ec864e64204c948e5bd2aa4b7ad65bf347772e2
basgoncalves <basilio.goncalves7@gmail.com>
2022-06-01 15:59:31 +0100

    update music classifier

--- Music_classifier/musicClassifier.py
+++ Music_classifier/musicClassifier.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import os
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-music_data = pd.read_csv('music.csv')
+print (os.getcwd())
+current_file = __file__
+music_data = pd.read_csv(os.path.join(current_file,'..\\music.csv'))
 X = music_data.drop(columns=['genre'])
 y = music_data['genre']
 X_train, X_test, y_train,y_test = train_test_split(X,y,test_size=0.2)
@@ -12,4 +15,4 @@ model = DecisionTreeClassifier()
 model.fit(X_train,y_train)
 predictions = model.predict(X_test)
 score = accuracy_score(y_test, predictions)
-score
+print('test accuracy = ', score)
