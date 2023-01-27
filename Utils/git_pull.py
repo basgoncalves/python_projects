a27870c3f6bda396c6ba07d485509b14d66a5ebb
basgoncalves <basilio.goncalves7@gmail.com>
2023-01-27 21:07:38 +0100

    update git pull

--- Utils/git_pull.py
+++ Utils/git_pull.py
@@ -10,15 +10,9 @@ device_details = platform.uname()
 print(type(device_details))
 msg = input('Which pc are you pulling to? (P = personal / W = work at uni vienna):') 
 
-<<<<<<< HEAD
 # list of directories where repos are stored depending on current local machine
 if msg is 'P':
         repos =[r'C:\Users\Bas\Documents\Papers-Reviews',
-=======
-if msg == 'P':
-        # list of repos in the current local machine
-        repos =[r'C:\Users\Bas\Documents\Papers-Reviews\Reviews',
->>>>>>> 950796954d55ddd4282a25cfcf9bd87a7379bb65
                 r'C:\Code\Git\Python_projects',
                 r'C:\Code\Git\MSKmodelling']
 elif msg == 'W':
