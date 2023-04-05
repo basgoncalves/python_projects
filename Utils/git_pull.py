e2fcef4c69b74ee4fb242424bac026a7499dfaa7
basgoncalves <basilio.goncalves7@gmail.com>
2023-04-05 15:43:30 +0200

    add reserach data  to git pull

--- Utils/git_pull.py
+++ Utils/git_pull.py
@@ -13,12 +13,14 @@ if device_details.node == 'Bas-PC':
         repos =[r'C:\Git\Papers-Reviews',
                 r'C:\Git\Python_projects',
                 r'C:\Git\MSKmodelling',
-                r'C:\Git\msk_modelling_python']
+                r'C:\Git\msk_modelling_python',
+                r'C:\Git\research_data']
 elif device_details.node == 'DESKTOP-8KRF896':
         repos =[r'C:\Git\Papers-Reviews',
                 r'C:\Git\Python_projects',
                 r'C:\Git\MSKmodelling',
-                r'C:\Git\msk_modelling_python']
+                r'C:\Git\msk_modelling_python',
+                r'C:\Git\research_data']
 else:
         print('Current machine not configured. Add folder paths to the script or perform "git pull manually"')
         exit()
