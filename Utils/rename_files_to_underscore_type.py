a78c4643b48cfdc459a89d47442c90c15671e10f
Bas <basilio.goncalves7@gmail.com>
2023-12-12 13:03:24 +0100

    add the option to look for all the current file types (to avoid files being mislabeld when adding underscore)

--- Utils/rename_files_to_underscore_type.py
+++ Utils/rename_files_to_underscore_type.py
@@ -2,6 +2,9 @@ import os
 from tkinter import filedialog
 import tkinter as tk
 import re
+import os
+from tkinter import filedialog
+import tkinter as tk
 
 def rename_files_and_folders(renaming_options):
     root = tk.Tk()
@@ -25,6 +28,37 @@ def rename_files_and_folders(renaming_options):
                     new_name = new_name.replace(old, new)
                 os.rename(old_name, new_name)
 
+
+def find_file_types(folder_path):
+    file_types = set()
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            _, file_ext = os.path.splitext(file)
+            file_types.add(file_ext)
+    return file_types
+
+def update_renaming_options(renaming_options):
+    root = tk.Tk()
+    root.withdraw()
+    folder_path = filedialog.askdirectory()
+    if not folder_path:
+        return renaming_options
+
+    def find_file_types(folder_path):
+        file_types = set()
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                _, file_ext = os.path.splitext(file)
+                file_types.add(file_ext)
+        return file_types
+
+    file_types = find_file_types(folder_path)
+    for file_type in file_types:
+        if file_type not in renaming_options.values():
+            new_key = "_" + file_type.replace(".", "")
+            renaming_options[new_key] = file_type
+    return renaming_options
+
 renaming_options = {
     "_osim": ".osim",
     "_jpg": ".jpg",
@@ -36,4 +70,11 @@ renaming_options = {
     "_fbx": ".fbx",
     "_blend": ".blend",
 }
+
+renaming_options = update_renaming_options(renaming_options)
+
+print(renaming_options)
+
 rename_files_and_folders(renaming_options)
+
+
