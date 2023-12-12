b65c61dbcef2ef0f34f5a246d9d4806aad333f4c
Bas <basilio.goncalves7@gmail.com>
2023-12-12 13:31:34 +0100

    update rename_files... (to make all files lower case)

--- Utils/rename_files_to_underscore_type.py
+++ Utils/rename_files_to_underscore_type.py
@@ -6,45 +6,7 @@ import os
 from tkinter import filedialog
 import tkinter as tk
 
-def rename_files_and_folders(renaming_options):
-    root = tk.Tk()
-    root.withdraw()
-    folder_path = filedialog.askdirectory()
-    if folder_path:
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                old_name = os.path.join(root, file)
-                file_name, file_ext = os.path.splitext(file)
-                words = re.findall(r'\w+', file_name)
-                new_name = os.path.join(root, '_'.join(word.lower() if word.isalpha() else word for word in words) + file_ext)
-                for old, new in renaming_options.items():
-                    new_name = new_name.replace(old, new)
-                os.rename(old_name, new_name)
-            for dir in dirs:
-                old_name = os.path.join(root, dir)
-                words = re.findall(r'\w+', dir)
-                new_name = os.path.join(root, '_'.join(word.lower() if word.isalpha() else word for word in words))
-                for old, new in renaming_options.items():
-                    new_name = new_name.replace(old, new)
-                os.rename(old_name, new_name)
-
-
 def find_file_types(folder_path):
-    file_types = set()
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            _, file_ext = os.path.splitext(file)
-            file_types.add(file_ext)
-    return file_types
-
-def update_renaming_options(renaming_options):
-    root = tk.Tk()
-    root.withdraw()
-    folder_path = filedialog.askdirectory()
-    if not folder_path:
-        return renaming_options
-
-    def find_file_types(folder_path):
         file_types = set()
         for root, dirs, files in os.walk(folder_path):
             for file in files:
@@ -52,12 +14,47 @@ def update_renaming_options(renaming_options):
                 file_types.add(file_ext)
         return file_types
 
+def update_renaming_options(folder_path, renaming_options):
+    
+    if not folder_path:
+        return renaming_options
+
     file_types = find_file_types(folder_path)
     for file_type in file_types:
         if file_type not in renaming_options.values():
             new_key = "_" + file_type.replace(".", "")
             renaming_options[new_key] = file_type
     return renaming_options
+
+
+def rename_files_and_folders(renaming_options):
+    root = tk.Tk()
+    root.withdraw()
+    folder_path = filedialog.askdirectory()
+    
+    renaming_options = update_renaming_options(folder_path, renaming_options)
+    
+    if folder_path:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                old_name = os.path.join(root, file)
+                file_name, file_ext = os.path.splitext(file)
+                words = re.findall(r'\w+', file_name)
+                new_name = os.path.join(root, '_'.join(word.lower() if word.isalpha() else word for word in words) + file_ext)
+                    
+                for old, new in renaming_options.items():
+                    new_name = new_name.replace(old, new)
+                new_name = new_name.lower()  # Convert new_name to lowercase
+                os.rename(old_name, new_name)
+                
+            for dir in dirs:
+                old_name = os.path.join(root, dir)
+                words = re.findall(r'[A-Z][a-z]*|[a-z]+|\d+', dir)
+                new_name = os.path.join(root, '_'.join(word.lower() for word in words))
+                for old, new in renaming_options.items():
+                    new_name = new_name.replace(old, new)
+                new_name = new_name.lower()  # Convert new_name to lowercase
+                os.rename(old_name, new_name)
 
 renaming_options = {
     "_osim": ".osim",
@@ -70,10 +67,6 @@ renaming_options = {
     "_fbx": ".fbx",
     "_blend": ".blend",
 }
-
-renaming_options = update_renaming_options(renaming_options)
-
-print(renaming_options)
 
 rename_files_and_folders(renaming_options)
 
