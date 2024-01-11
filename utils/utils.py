2ce83eb60a45aba10561f999b2c14af200c96f73
basgoncalves <basilio.goncalves7@gmail.com>
2024-01-11 11:15:28 +0100

    ** utils: update utils.py to prompt with all the options available

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,15 @@
 import sys
 import os
 
+# create a class for each option so that we can print the option names
+class Option:
+    def __init__(self, func):
+        self.func = func
+
+    def run(self):
+        self.func()
+
+#%% Options
 def speed_test():
     import speedtest
     speed_test = speedtest.Speedtest()
@@ -11,10 +20,35 @@ def speed_test():
     upload_speed = round(speed_test.upload()/1e6)
     print("Your Upload speed is", upload_speed,'Mb')
 
+def get_current_dir():
+    print("for .py")
+    print("dir_path = os.path.dirname(os.path.realpath(__file__))")
+    print("for ipynb")
+    print("dir_path = os.getcwd()")
+
+def print_python_path():
+    print(sys.executable)
+
+def print_python_libs():
+    print(os.path.join(os.path.dirname(sys.executable),'Lib','site-packages'))
+
+# print names (NOT AN OPTION)
+def print_option_names():
+    options = [name for name in globals() if isinstance(globals()[name], Option)]
+    for option in options:
+        print(option)
+
+#%% Errors
+def print_error_message():
+    print("please select one of the following options:")
+    print_option_names()
+    print("eg usage: utils.py speet_test")
+
+#%% Select
 def select():
     # Check if the number of command line arguments is not equal to 2
     if len(sys.argv) != 2:
-        print("usage: setup.py option1 or option2")
+        print_error_message()
         sys.exit(1)
 
     # Get the command line argument
@@ -22,18 +56,21 @@ def select():
 
     # Check the value of the command line argument
     if option == "python_path":
-        print(sys.executable)
+        print_python_path()
 
     elif option == "python_libs":
-        print(os.path.join(os.path.dirname(sys.executable),'Lib','site-packages'))
+        print_python_libs()
 
     elif option == "speed_test":
         speed_test()
 
     else:
         # Invalid command line argument
-        print("usage: setup.py option1 or option2")
+        print_error_message()
         sys.exit(1)
+
+speed_test = Option(speed_test)
+get_current_dir = Option(get_current_dir)
 
 if __name__ == "__main__":
     select()
